57a8119ad34ae03ca52e5e41db6d7fabdf553a79
Jamil54 <77116891+Jamil54@users.noreply.github.com>
2021-01-07 20:02:55 +0300

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+username = input('Enter your username: ')
+print('Welcome: ',username)
+
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+sum = float(num1) + float(num2)
+print(sum)
+
+
+# In[ ]:
+
+
+
+
